f525938c3442859100fdaa6454bf55f0867b01e7
Vishav Premlall <VishavP@sahomeloans.com>
2015-02-03 17:00:28 +0200

    Added method to return a string to display

--- Hello.cs
+++ Hello.cs
@@ -1,7 +1,12 @@
 using System;
 
 class Hello {
+
+	private static string GetText(){
+		return "Hello World";
+	}
+
 	public static void Main(){
-		Console.WriteLine("Hi");
+		Console.WriteLine(GetText());
 	}
 }
