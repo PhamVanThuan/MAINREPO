d82e1786b522ac32286ead05042cb36e17b9ce22
Vishav Premlall <VishavP@sahomeloans.com>
2015-02-06 09:56:43 +0200

    Added a sum method with reference parameters

--- vs2013_ci/CITester/CITester/Program.cs
+++ vs2013_ci/CITester/CITester/Program.cs
@@ -8,6 +8,12 @@ namespace CITester
 {
     class Program
     {
+        
+        public int sum(ref int a, ref int b)
+        {
+            return a + b;
+        }
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello CI");
@@ -15,7 +21,10 @@ namespace CITester
             Program obj = new Program();
             string testString = "Created on VS_2013";
             Console.WriteLine(testString);
-            
+
+            int one = 1;
+            int two = 2;
+            obj.sum(ref one,ref two);       //same as obj.sum(&one,&two) in C++
         }
     }
 }
