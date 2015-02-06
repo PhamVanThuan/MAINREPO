7064438ebdb191f9f7111f814925c83ea72fc1bd
Vishav Premlall <VishavP@sahomeloans.com>
2015-02-06 09:36:01 +0200

    My first console app on vs 2013 :D

---
+++ vs2013_ci/CITester/CITester/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CITester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello CI");
+
+            Program obj = new Program();
+            string testString = "Created on VS_2013";
+            Console.WriteLine(testString);
+            
+        }
+    }
+}
