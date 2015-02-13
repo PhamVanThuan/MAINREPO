f1bbe633a9a7cc108de133579b21264fbff97482
Vishav Premlall <VishavP@sahomeloans.com>
2015-02-13 07:41:38 +0200

    -Changes to unit test files

---
+++ vs2013_ci/ClassLibrary_UnitTestingExcercise/ClassLibrary_UnitTestingExcercise/AccountBalanceService.cs
@@ -0,0 +1,33 @@
+﻿  1  ﻿using System;
+  2  using System.Collections.Generic;
+  3  using System.Linq;
+  4  using System.Text;
+  5  using BusinessObjects.Entities;
+  6  using System.Security;
+  7  
+  8  namespace BusinessObjects.Services
+  9  {
+ 10      public class AccountBalanceService
+ 11      {
+ 12          private IAverageJoeBankService _averageJoeService;
+ 13  
+ 14          public AccountBalanceService(IAverageJoeBankService averageJoeService)
+ 15          {
+ 16              _averageJoeService = averageJoeService;
+ 17          }
+ 18  
+ 19          public double GetAccountBalanceByUser(User user)
+ 20          {       
+ 21              // the authenticate method below takes too much time! 
+ 22              bool isAuthenticated = _averageJoeService.Authenticate(user.UserName, user.Password);
+ 23  
+ 24              if (!isAuthenticated)
+ 25                  throw new SecurityException("User is not authenticated"); 
+ 26              
+ 27              // access database using username and get the balance 
+ 28  
+ 29              return 100;      
+ 30          }
+ 31  
+ 32      
+ 33  }
---
+++ vs2013_ci/ClassLibrary_UnitTestingExcercise/ClassLibrary_UnitTestingExcercise/AverageJoeBankService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary_UnitTestingExcercise
+{
+    public class AverageJoeBankService : IAverageJoeBankService
+    {
+        public bool Authenticate(string userName, string password)
+        {
+            // This is just simulate the time taken for the web service to authenticate! 
+            System.Threading.Thread.Sleep(5000);
+            return true;
+        }
+    }
+}
