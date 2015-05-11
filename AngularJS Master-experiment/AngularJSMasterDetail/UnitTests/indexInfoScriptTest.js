2d7ef4afc19510c89e200cebe8ffa7b0e8911243
VishavPremlall <VishavP@sahomeloans.com>
2015-05-11 14:18:22 +0200

    new node modules

--- AngularJS Master-experiment/AngularJSMasterDetail/UnitTests/indexInfoScriptTest.js
+++ AngularJS Master-experiment/AngularJSMasterDetail/UnitTests/indexInfoScriptTest.js
@@ -12,8 +12,4 @@
             $scope: scope
         });
     }));
-
-    it('should attach a list of awesomeThings to the scope', function () {
-        expect(scope.awesomeThings.length).toBe(3);
-    });
 });
