20d52abfd118cced6e3241fcf1b2fe7000df11a0
VishavPremlall <VishavP@sahomeloans.com>
2015-05-13 14:21:47 +0200

    Modified error message

--- AngularJS Master-experiment/AngularJSMasterDetail/specs/validation.spec.js
+++ AngularJS Master-experiment/AngularJSMasterDetail/specs/validation.spec.js
@@ -3,8 +3,7 @@
 function validateForm() {
     var x = document.forms["angularApp"][MyCalc].value;
     if (x == null || x == "" || x<=0 ) {
-        alert("Cannot equal to or be less than 0");
-        alert("Cannot contain alpha characters");
+        alert("Invalid input, it cannot equal to or be less than 0");
         return false;
     }
 }
