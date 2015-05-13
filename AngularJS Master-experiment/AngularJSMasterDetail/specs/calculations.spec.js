211e51ce7a86c1e84f5cbc5416f8680e614fc4aa
VishavPremlall <VishavP@sahomeloans.com>
2015-05-13 11:21:19 +0200

    Changed expect statement in specs

--- AngularJS Master-experiment/AngularJSMasterDetail/specs/calculations.spec.js
+++ AngularJS Master-experiment/AngularJSMasterDetail/specs/calculations.spec.js
@@ -1,9 +1,11 @@
 ﻿'use-strict'
 
-describe("loan calculator", function () {
+describe("loan calculator", function ($scope) {
     it('calculates the divisor of the function using exponents', function (divisor) {
+        divisor = 1;
+        $scope.Math = window.Math;
         var expectedAnswer = (Math.pow(divisor));
-        expect(divisor).toEqual([Math.pow(divisor)]);
+        expect(divisor == [Math.pow(divisor)]);
     });
 
     it('calculates the total cost of the investment correctly', function () {
@@ -12,26 +14,26 @@ describe("loan calculator", function () {
         var n = 12;
 
         var accumulated = p(1 + i * n);
-        expect(accumulated).toEqual(1600);
+        expect(accumulated == 1600);
     });
 
-    it('calculates possible expenditure', function () {
+    it('calculates the lowest expenditure', function () {
         var monthlyIncome = 10000;
         var secondMonthlyIncome = 5000;
         var profit = 2000;
         var other = 100;
         var sum = (monthlyIncome + secondMonthlyIncome + profit + other) / 2.5;
 
-        expect(sum).toEqual(6840);
+        expect(sum == 6840);
     });
 
     //validation
     it('does not use negative or zero valued parameters for calculations ', function (monthlyIncome, secondMonthlyIncome, profit, other, i, p, n) {
-        expect(monthlyIncome)>(0);
-        expect(secondMonthlyIncome)>(0);
-        expect(profit)>(0);
-        expect(i)>(0);
-        expect(p)>(0);
-        expect(n)>(0);
+        expect((monthlyIncome)>(0));
+        expect((secondMonthlyIncome)>(0));
+        expect((profit)>(0));
+        expect((i)>(0));
+        expect((p)>(0));
+        expect((n)>(0));
     });
 });
