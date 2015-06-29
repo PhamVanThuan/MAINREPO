a1eeac71cc1c55eaa05dc5f2403377862d2b4c29
VishavPremlall <VishavP@sahomeloans.com>
2015-06-29 08:25:24 +0200

    Updated spec

--- ServicePinger/ServicePinger/Scripts/ControllerSpec.js
+++ ServicePinger/ServicePinger/Scripts/ControllerSpec.js
@@ -12,31 +12,59 @@ describe('Controller: HomeCtrl', function () {
         });
     }));
 
-    var AboutCtrl, scope;
-    beforeEach(inject(function ($scontroller, $rootscope) {
-        scope = $rootscope.$new();
+    it('should attach items to the scope', function () {
+        expect(scope.awesomeThings.length).toBe(3);
+    });
+});
+
+describe('Controller: AboutCtrl', function () {
+    beforeEach(module('ServicePinger'));
+    var AboutCtrl,
+      scope;
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
         AboutCtrl = $controller('AboutCtrl', {
             $scope: scope
+
         });
     }));
 
-    var LoginCtrl, scope;
-    beforeEach(inject(function ($scontroller, $rootscope) {
-        scope = $rootscope.$new();
+    it('should attach items to the scope', function () {
+        expect(scope.awesomeThings.length).toBe(3);
+    });
+});
+
+describe('Controller: LoginCtrl', function () {
+    beforeEach(module('ServicePinger'));
+    var LoginCtrl,
+      scope;
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
         LoginCtrl = $controller('LoginCtrl', {
             $scope: scope
         });
     }));
 
-    var Error404Ctrl, scope;
-    beforeEach(inject(function ($scontroller, $rootscope) {
-        scope = $rootscope.$new();
+    it('should attach items to the scope', function () {
+        expect(scope.awesomeThings.length).toBe(3);
+    });
+});
+
+describe('Controller: Error404Ctrl', function () {
+    beforeEach(module('ServicePinger'));
+    var HomeCtrl,
+      scope;
+
+    beforeEach(inject(function ($controller, $rootScope) {
+        scope = $rootScope.$new();
         Error404Ctrl = $controller('Error404Ctrl', {
             $scope: scope
         });
     }));
 
     it('should attach items to the scope', function () {
-        expect(scope.awesomeThings.length).isNot(0);
+        expect(scope.awesomeThings.length).toBe(3);
     });
 });
