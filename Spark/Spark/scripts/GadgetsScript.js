04000324f90a54bddfb089017df4723bfbfe5930
VishavPremlall <VishavP@sahomeloans.com>
2015-07-08 10:35:09 +0200

    Added some odd things

---
+++ Spark/Spark/scripts/GadgetsScript.js
@@ -0,0 +1,10 @@
+﻿(function () {
+
+    var app = angular.module("app",[]);
+
+    var dateCtrl = function ($scope) {
+        $scope.date = new Date();
+    };
+
+    app.controller("dateCtrl", ["$scope", dateCtrl]);
+})();
