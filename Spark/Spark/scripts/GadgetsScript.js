ff39781d0538663a0edc6677060ceeefe850363e
VishavPremlall <VishavP@sahomeloans.com>
2015-07-08 13:29:33 +0200

    Added json data file

--- Spark/Spark/scripts/GadgetsScript.js
+++ Spark/Spark/scripts/GadgetsScript.js
@@ -1,10 +1,1 @@
-﻿(function () {
-
-    var app = angular.module("app",[]);
-
-    var dateCtrl = function ($scope) {
-        $scope.date = new Date();
-    };
-
-    app.controller("dateCtrl", ["$scope", dateCtrl]);
-})();
+﻿
--- Spark/Spark/scripts/index.js
+++ Spark/Spark/scripts/index.js
@@ -10,16 +10,28 @@
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
+        document.addEventListener('resume', onResume.bind(this), false);
+        document.addEventListener("batterycritical", onBatteryCritical, false);
+
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
+
+    function onBatteryCritical(info) {
+        // Handle the battery critical event
+        alert("Battery Level Critical " + info.level + "%\nRecharge Soon!");
+    };
+
     function onPause() {
-        // TODO: This application has been suspended. Save application state here.
+        // TODO: This application has been suspended. Save application state here. Handle the on pause event
+        setTimeout(function () {
+            // TODO: do your thing!
+        }, 0);
     };
 
     function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
+        // TODO: This application has been reactivated. Restore application state here. Handle the resume event
     };
+
+
 } )();
