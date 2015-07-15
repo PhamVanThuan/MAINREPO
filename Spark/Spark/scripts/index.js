38ef0ad16bb448deb2b2f1cbcde0781f79b707c6
VishavPremlall <VishavP@sahomeloans.com>
2015-07-15 11:14:52 +0200

    Fixed navigation

--- Spark/Spark/scripts/index.js
+++ Spark/Spark/scripts/index.js
@@ -9,21 +9,22 @@
 
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
-        document.addEventListener( 'pause', onPause.bind( this ), false );
+        document.addEventListener('pause', onPause.bind( this ), false );
         document.addEventListener('resume', onResume.bind(this), false);
         document.addEventListener("batterycritical", onBatteryCritical, false);
 
         // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
     };
 
-
     function onBatteryCritical(info) {
         // Handle the battery critical event
         alert("Battery Level Critical " + info.level + "%\nRecharge Soon!");
+        onPause(" Due to a low battery.");
     };
 
     function onPause() {
         // TODO: This application has been suspended. Save application state here. Handle the on pause event
+        alert("Application has been paused "+onPause());
         setTimeout(function () {
             // TODO: do your thing!
         }, 0);
@@ -32,6 +33,5 @@
     function onResume() {
         // TODO: This application has been reactivated. Restore application state here. Handle the resume event
     };
-
 
 } )();
