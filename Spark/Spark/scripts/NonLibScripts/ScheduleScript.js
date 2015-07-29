c3ebcc354695938eecc1cabe3a21ffad41703bc6
VishavPremlall <VishavP@sahomeloans.com>
2015-07-29 10:09:25 +0200

    Added schedules mocked file

---
+++ Spark/Spark/scripts/NonLibScripts/ScheduleScript.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    var app = angular.module("app", []);
+    var MainController = function (name, $scope) {
+        $scope.name = name; //username
+        alert("Welcome " + name);
+    }
+
+    app.controller("MainController", MainController);
+
+    var DateController = function ($scope) {
+
+        $scope.date = new Date();
+    };
+
+    app.controller("DateController", DateController);
+
+})();
