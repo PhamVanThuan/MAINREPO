2d57730065d2b02076bc89f3a41555bdff5132b4
VishavPremlall <VishavP@sahomeloans.com>
2015-08-19 10:49:10 +0200

    Fixed ripple emulation bug

--- Spark/Spark/scripts/NonLibScripts/login.js
+++ Spark/Spark/scripts/NonLibScripts/login.js
@@ -3,7 +3,7 @@ function login(form) {
     /*the following code checkes whether the entered userid and password are matching*/
     if (form.username.value == "Suhail" && form.password.value == "Suhail01" || form.username.value == "Lesley" && form.password.value == "Lesley01" || form.username.value == "Viresh" && form.password.value == "Viresh01" || form.username.value == "Vishav" && form.password.value == "Vishav01")
     {
-        window.open('schedule.html')/*opens the target page while Id & password matches*/
+        window.open('schedule.html?enableripple=cordova-3.0.0-NexusGalaxy') /*opens the target page while Id & password matches*/
     }
     else {
         /*alert("The Password or Username Does Not Match!!!")displays error message*/
