4470ea30583c270627ba423050df3f9d09709439
VishavPremlall <VishavP@sahomeloans.com>
2015-08-19 10:18:26 +0200

    Backup

--- Spark/Spark/scripts/NonLibScripts/ScheduleScript.js
+++ Spark/Spark/scripts/NonLibScripts/ScheduleScript.js
@@ -1,14 +1,47 @@
 ﻿(function () {
     var app = angular.module("app", []);
 
+
     var DateController = function ($scope) {
-        $scope.rowCollection = [{area : 'Avoca', time: '8-00'}, 
-                                {area : 'Pheonix', time : '10:00'},
-                                { area: 'Verulam', time: '12-00' }];
+        $scope.rowCollection = [{ area: 'Asherville', time: '18:00', etime: '20:30' },
+    { area: 'Atholl Heights', time: '20:00', etime: '22:30' },
+    { area: 'Avoca Hills', time: '08:00', etime: '10:30' },
+    { area: 'Bluff', time: '12:00', etime: '14:30' },
+    { area: 'Bothas Hill', time: '14:00', etime: '16:30' },
+    { area: 'Durban North', time: '16:00', etime: '18:30' },
+    { area: 'Hillcrest', time: '14:00', etime: '16:30' },
+    { area: 'La Lucia North', time: '06:00', etime: '08:30' },
+    { area: 'Lamontville', time: '06:00', etime: '08:30' },
+    { area: 'Sydenham', time: '14:00', etime: '16:30' },
+    { area: 'Umlazi', time: '04:00', etime: '06:30' },
+    { area: 'Waterfall', time: '20:00', etime: '22:30' },
+    { area: 'Woodview', time: '18:00', etime: '22:30' },
+    { area: 'Mobeni Heights', time: '20:00', etime: '22:30' },
+    { area: 'New Germany', time: '18:00', etime: '20:30' }
+
+        ];
+
+        $scope.search = function (area) { //call this function from ng click on submit button
+
+            var arr = [{ area: 'Asherville', time: '18:00', etime: '20:30' },
+                                    { area: 'Atholl Heights', time: '20:00', etime: '22:30' },
+                                    { area: 'Avoca Hills', time: '08:00', etime: '10:30' },
+   { area: 'Bluff', time: '12:00', etime: '14:30' },
+   { area: 'Bothas Hill', time: '14:00', etime: '16:30' },
+   { area: 'Durban North', time: '16:00', etime: '18:30' },
+   { area: 'Hillcrest', time: '14:00', etime: '16:30' },
+   { area: 'La Lucia North', time: '06:00', etime: '08:30' },
+   { area: 'Lamontville', time: '06:00', etime: '08:30' },
+   { area: 'Sydenham', time: '14:00', etime: '16:30' },
+   { area: 'Umlazi', time: '04:00', etime: '06:30' },
+   { area: 'Waterfall', time: '20:00', etime: '22:30' },
+   { area: 'Woodview', time: '18:00', etime: '20:30' },
+   { area: 'Mobeni Heights', time: '20:00', etime: '22:30' },
+   { area: 'New Germany', time: '18:00', etime: '20:30' }
+
+            ];
+        };//controller
     };
+        app.controller("DateController", DateController);
 
-    app.controller("DateController", DateController);
-
-
-
-})();
+    })();
---
+++ Spark/Spark/scripts/NonLibScripts/login.js
@@ -0,0 +1,12 @@
+﻿/*function to check userid & password*/
+function login(form) {
+    /*the following code checkes whether the entered userid and password are matching*/
+    if (form.username.value == "Suhail" && form.password.value == "Suhail01" || form.username.value == "Lesley" && form.password.value == "Lesley01" || form.username.value == "Viresh" && form.password.value == "Viresh01" || form.username.value == "Vishav" && form.password.value == "Vishav01")
+    {
+        window.open('schedule.html')/*opens the target page while Id & password matches*/
+    }
+    else {
+        /*alert("The Password or Username Does Not Match!!!")displays error message*/
+        document.getElementById('loginerror').innerHTML = "The Password or Username Do Not Match!!!";
+    }
+}
